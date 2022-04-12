d17de4f13664fd87b248eef3e115eb28f92ab8f6
Rebecca-Stone <primstone16@gmail.com>
2022-04-12 18:36:47 -0500

    got a few more tests to pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,40 +1,71 @@
 // implement your posts router here
-const express = require('express');
-const Post = require('./posts-model');
+const express = require("express");
+const Post = require("./posts-model");
 
 const router = express.Router();
 
-
 // #### 1 [GET] /api/posts
-router.get('/', (req, res) => {
-    Post.find()
-        .then(posts => {
-            res.status(200).json(posts)
-        })
-        .catch(() => {
-            res.status(500).json({ message: "The posts information could not be retrieved" })
-        })
-})
+router.get("/", (req, res) => {
+  Post.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
+});
 
 // #### 2 [GET] /api/posts/:id
 
-// - If the _post_ with the specified `id` is not found:
-
-//   - return HTTP status code `404` (Not Found).
-//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
-
-// - If there's an error in retrieving the _post_ from the database:
-//   - respond with HTTP status code `500`.
-//   - return the following JSON: `{ message: "The post information could not be retrieved" }`.
-
-
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
+});
 
 // #### 3 [POST] /api/posts
+router.post("/", (req, res) => {
+  let title = req.body.title;
+  let contents = req.body.contents;
+  let newPost = {
+    title: title,
+    contents: contents,
+  };
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.insert(newPost)
+      .then((newPost) => {
+        res.status(201).json(newPost);
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
+});
 
 // - If the request body is missing the `title` or `contents` property:
 
 //   - respond with HTTP status code `400` (Bad Request).
-//   - return the following JSON: `{ message: "Please provide title and contents for the post" }`.
+//   - return the following JSON: `{  }`.
 
 // - If the information about the _post_ is valid:
 
@@ -44,9 +75,7 @@ router.get('/', (req, res) => {
 
 // - If there's an error while saving the _post_:
 //   - respond with HTTP status code `500` (Server Error).
-//   - return the following JSON: `{ message: "There was an error while saving the post to the database" }`.
-
-
+//   - return the following JSON: `{  }`.
 
 // #### 4 [PUT] /api/posts/:id
 
@@ -71,8 +100,6 @@ router.get('/', (req, res) => {
 //   - return HTTP status code `200` (OK).
 //   - return the newly updated _post_.
 
-
-
 // #### 5 [DELETE] /api/posts/:id
 
 // - If the _post_ with the specified `id` is not found:
@@ -84,8 +111,6 @@ router.get('/', (req, res) => {
 
 //   - respond with HTTP status code `500`.
 //   - return the following JSON: `{ message: "The post could not be removed" }`.
-
-
 
 // #### 6 [GET] /api/posts/:id/comments
 
