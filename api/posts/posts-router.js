366a56aa1d200b9200ce7404557a20c9845682b2
Rebecca-Stone <primstone16@gmail.com>
2022-04-12 20:50:15 -0500

    completed more tasks

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,7 +18,6 @@ router.get("/", (req, res) => {
 });
 
 // #### 2 [GET] /api/posts/:id
-
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
     .then((post) => {
@@ -39,20 +38,19 @@ router.get("/:id", (req, res) => {
 
 // #### 3 [POST] /api/posts
 router.post("/", (req, res) => {
-  let title = req.body.title;
-  let contents = req.body.contents;
-  let newPost = {
-    title: title,
-    contents: contents,
-  };
+  let { title, contents } = req.body;
+
   if (!title || !contents) {
     res
       .status(400)
       .json({ message: "Please provide title and contents for the post" });
   } else {
-    Post.insert(newPost)
-      .then((newPost) => {
-        res.status(201).json(newPost);
+    Post.insert({ title, contents })
+      .then(({ id }) => {
+        return Post.findById(id);
+      })
+      .then((post) => {
+        res.status(201).json(post);
       })
       .catch(() => {
         res.status(500).json({
@@ -62,37 +60,41 @@ router.post("/", (req, res) => {
   }
 });
 
-// - If the request body is missing the `title` or `contents` property:
-
-//   - respond with HTTP status code `400` (Bad Request).
-//   - return the following JSON: `{  }`.
-
-// - If the information about the _post_ is valid:
-
-//   - save the new _post_ the the database.
-//   - return HTTP status code `201` (Created).
-//   - return the newly created _post_.
-
-// - If there's an error while saving the _post_:
-//   - respond with HTTP status code `500` (Server Error).
-//   - return the following JSON: `{  }`.
-
 // #### 4 [PUT] /api/posts/:id
 
-// - If the _post_ with the specified `id` is not found:
+router.put("/:id", (req, res) => {
+  let { title, contents } = req.body;
 
-//   - return HTTP status code `404` (Not Found).
-//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
-
-// - If the request body is missing the `title` or `contents` property:
-
-//   - respond with HTTP status code `400` (Bad Request).
-//   - return the following JSON: `{ message: "Please provide title and contents for the post" }`.
-
-// - If there's an error when updating the _post_:
-
-//   - respond with HTTP status code `500`.
-//   - return the following JSON: `{ message: "The post information could not be modified" }`.
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.findById(req.params.id)
+      .then((post) => {
+        if (!post) {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist" });
+        } else {
+          return Post.update(req.params.id, req.body);
+        }
+      })
+      .then((post) => {
+        if (post) {
+          return Post.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        res.status(200).json(post);
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "The post information could not be modified" });
+      });
+  }
+});
 
 // - If the post is found and the new information is valid:
 
@@ -101,18 +103,29 @@ router.post("/", (req, res) => {
 //   - return the newly updated _post_.
 
 // #### 5 [DELETE] /api/posts/:id
-
-// - If the _post_ with the specified `id` is not found:
-
-//   - return HTTP status code `404` (Not Found).
-//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
-
-// - If there's an error in removing the _post_ from the database:
-
-//   - respond with HTTP status code `500`.
-//   - return the following JSON: `{ message: "The post could not be removed" }`.
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      await Post.remove(req.params.id);
+      res.json(post);
+    }
+  } catch (err) {
+    res.status(500).json({ message: "The post could not be removed" });
+  }
+});
 
 // #### 6 [GET] /api/posts/:id/comments
+// router.get('/:id/comments', (req, res) => {
+//     Post.findCommentById(req.params.id)
+//         .then(post => {
+//             if()
+//         })
+// })
 
 // - If the _post_ with the specified `id` is not found:
 
