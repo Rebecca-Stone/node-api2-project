65a4e739751576f80420b41e84bb5c4c9a37afab
Rebecca-Stone <primstone16@gmail.com>
2022-04-12 20:59:30 -0500

    completed all tasks

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,8 @@
-// implement your posts router here
 const express = require("express");
 const Post = require("./posts-model");
 
 const router = express.Router();
 
-// #### 1 [GET] /api/posts
 router.get("/", (req, res) => {
   Post.find()
     .then((posts) => {
@@ -17,7 +15,6 @@ router.get("/", (req, res) => {
     });
 });
 
-// #### 2 [GET] /api/posts/:id
 router.get("/:id", (req, res) => {
   Post.findById(req.params.id)
     .then((post) => {
@@ -36,7 +33,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// #### 3 [POST] /api/posts
 router.post("/", (req, res) => {
   let { title, contents } = req.body;
 
@@ -59,8 +55,6 @@ router.post("/", (req, res) => {
       });
   }
 });
-
-// #### 4 [PUT] /api/posts/:id
 
 router.put("/:id", (req, res) => {
   let { title, contents } = req.body;
@@ -96,13 +90,6 @@ router.put("/:id", (req, res) => {
   }
 });
 
-// - If the post is found and the new information is valid:
-
-//   - update the post document in the database using the new information sent in the `request body`.
-//   - return HTTP status code `200` (OK).
-//   - return the newly updated _post_.
-
-// #### 5 [DELETE] /api/posts/:id
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -119,22 +106,22 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-// #### 6 [GET] /api/posts/:id/comments
-// router.get('/:id/comments', (req, res) => {
-//     Post.findCommentById(req.params.id)
-//         .then(post => {
-//             if()
-//         })
-// })
-
-// - If the _post_ with the specified `id` is not found:
-
-//   - return HTTP status code `404` (Not Found).
-//   - return the following JSON: `{ message: "The post with the specified ID does not exist" }`.
-
-// - If there's an error in retrieving the _comments_ from the database:
-
-//   - respond with HTTP status code `500`.
-//   - return the following JSON: `{ message: "The comments information could not be retrieved" }`.
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const messages = await Post.findPostComments(req.params.id);
+      res.json(messages);
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
+  }
+});
 
 module.exports = router;
